dcec15aa83adb7769f2afe3ceaca5790bab627ef
vivekkothari <vivekkothari2304@gmail.com>
2025-05-17 15:21:27 +0530

    Use go jet

--- go.mod
+++ go.mod
@@ -5,10 +5,20 @@ go 1.24
 require github.com/jackc/pgx/v5 v5.7.4
 
 require (
+	github.com/go-jet/jet/v2 v2.13.0
+	github.com/lib/pq v1.10.9
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	golang.org/x/crypto v0.35.0 // indirect
-	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ repository/url_repository_test.go
@@ -0,0 +1,67 @@
+package repository
+
+import (
+	"context"
+	"testing"
+	"time"
+	"url-shortner/db"
+
+	_ "github.com/lib/pq"
+)
+
+func TestInsertAndGetURL(t *testing.T) {
+	database, err := db.NewDatabase()
+	repo := NewURLRepository(database)
+
+	original := "https://example.com"
+
+	id, err := repo.SaveURL(context.Background(), original)
+	if err != nil {
+		t.Fatalf("InsertURL failed: %v", err)
+	}
+
+	url, err := repo.getLongUrl(context.Background(), id)
+	if err != nil {
+		t.Fatalf("Unable to fetch url with id %d", id)
+	}
+
+	if url.LongURL != original {
+		t.Fatalf("Unable to match %s and %s", url.LongURL, original)
+	}
+
+}
+
+func TestInsertAndAccessUrl(t *testing.T) {
+	database, err := db.NewDatabase()
+	repo := NewURLRepository(database)
+
+	original := "https://example123.com"
+
+	id, err := repo.SaveURL(context.Background(), original)
+	if err != nil {
+		t.Fatalf("InsertURL failed: %v", err)
+	}
+
+	now := time.Now()
+
+	longURL, err := repo.AccessLongURL(context.Background(), id, now)
+
+	if err != nil {
+		t.Fatalf("Unable to fetch url with id %d", id)
+	}
+
+	url, err := repo.getLongUrl(context.Background(), id)
+
+	if err != nil {
+		t.Fatalf("Unable to fetch url with id %d", id)
+	}
+
+	if !url.LastAccessedAt.Equal(now) {
+		t.Fatalf("last accessed at not test")
+	}
+
+	if longURL != original {
+		t.Fatalf("Unable to match %s and %s", longURL, original)
+	}
+
+}
